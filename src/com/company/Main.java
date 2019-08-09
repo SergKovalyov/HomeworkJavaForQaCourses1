e0bbcd3f5d80566ff8d0a37b467644662a7f1dcf
Serhii Kovalov <Serg.kovalyov81@gmail.com>
2019-08-09 09:58:37 +0300

    Commit6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Test for GIT");
+        System.out.println("Test 2 for GIT");
     }
 }
