2159dbb07000284844a5113c506fd17cfe2d2d90
Serhii Kovalov <Serg.kovalyov81@gmail.com>
2019-08-13 13:18:42 +0300

    Commit 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -140,6 +140,8 @@ public class Main {
             }
             System.out.println("HI");
             System.out.println("HI");
+            System.out.println("HI");
+            System.out.println("HI");
         }
     }
 }
